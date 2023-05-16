e581c857aea5322cf17481d979cae31890bbada4
Changlin Li <mail@changlinli.com>
2023-05-16 13:18:11 -0400

    Add in check_cell function

--- game_of_life.py
+++ game_of_life.py
@@ -1,7 +1,7 @@
 ALIVE = 'O'
 DEAD = 'X'
 
-grid = [
+top_grid = [
     [ALIVE, ALIVE, DEAD, ALIVE],
     [ALIVE, ALIVE, DEAD, ALIVE],
     [ALIVE, DEAD, DEAD, ALIVE],
@@ -11,8 +11,36 @@ grid = [
 def is_live(cell):
     return cell == ALIVE
 
-def check_two_live_neighbours(x, y):
-    all_neighbours = [(neighbour_x, neighbour_y) for neighbour_x, neighbour_y in ...]
+def get_neighbour_values(x, y, grid):
+    all_neighbour_coordinates = \
+        [(neighbour_x, neighbour_y) for neighbour_x in range(x - 1, x + 2) for neighbour_y in range(y - 1, y + 2) if
+         neighbour_x >= 0 and neighbour_y >= 0]
+    print(all_neighbour_coordinates)
+    all_neighbour_values = [ grid[neighbour_x][neighbour_y] for (neighbour_x, neighbour_y) in all_neighbour_coordinates ]
+    return all_neighbour_values
+
+def check_cell(x, y, grid):
+    all_live_neighbours = [neighbour for neighbour in get_neighbour_values(x, y, grid) if is_live(neighbour)]
+    num_of_live_neighbours = len(all_live_neighbours)
+    if is_live(grid[x][y]):
+        if num_of_live_neighbours < 2:
+            return DEAD
+        elif num_of_live_neighbours >= 2 and num_of_live_neighbours <= 3:
+            return ALIVE
+        elif num_of_live_neighbours > 3:
+            return DEAD
+    else:
+        if num_of_live_neighbours == 3:
+            return ALIVE
+        else:
+            return DEAD
+
+
+def check_two_live_neighbours(x, y, grid):
+    all_live_neighbours = [ neighbour for neighbour in get_neighbour_values(x, y, grid) if is_live(neighbour) ]
+
+
+print(get_neighbour_values(2, 2, top_grid))
 
 
 
